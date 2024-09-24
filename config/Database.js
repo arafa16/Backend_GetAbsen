2bf9b107b464da558d2ca09157ce9a49a46c2266
ara fa adri <arafa.sco@gmail.com>
2024-09-24 15:57:30 +0700

    update timezone

--- config/Database.js
+++ config/Database.js
@@ -5,7 +5,8 @@ dotenv.config();
 
 const db = new sequelize(process.env.DATABASE_NAME, process.env.DATABASE_USER, process.env.DATABASE_PASSWORD,{
     host:process.env.DATABASE_HOST,
-    dialect: process.env.DATABASE_DIALECT
+    dialect: process.env.DATABASE_DIALECT,
+    timezone: '+00:00'
 });
 
 export default db;
